1e5708a086aefaf555676c529e774fe6858b411e
Nguyen Thanh Duc <DucNguyen@techmasters-mac-pro.local>
2015-10-09 17:59:46 +0700

    Initial Commit

---
+++ ObjCExamples/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCExamples
+//
+//  Created by Nguyen Thanh Duc on 10/9/15.
+//  Copyright © 2015 Nguyen Thanh Duc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjCExamples/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ObjCExamples
+//
+//  Created by Nguyen Thanh Duc on 10/9/15.
+//  Copyright © 2015 Nguyen Thanh Duc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
